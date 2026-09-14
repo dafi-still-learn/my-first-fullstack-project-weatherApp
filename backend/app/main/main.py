72544733c6020ff96f57f58966cde4e84ec2c718
dafi <khadafi291105@gmail.com>
2026-09-14 23:41:21 +0700

    memperbaiki beberapa bug cors

--- backend/app/main/main.py
+++ backend/app/main/main.py
@@ -11,6 +11,7 @@ app.include_router(router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
+                   "https://dafi-still-learn-my-first-fullstack.vercel.app",
                    "https://dafi-still-learn-my-first-fullstack.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
